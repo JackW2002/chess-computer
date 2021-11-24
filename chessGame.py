661f0b375b777bfae2830b6ad467b07bdc3b9c9a
JackW002 <Ch0nker0fAPassw0rd2169>
2021-11-24 17:57:48 +0000

    code reformatting

--- chessGame.py
+++ chessGame.py
@@ -3,53 +3,55 @@ from pieces import *
 
 # Loads a board with the given FEN string
 def loadPos(fen: str):
-    board = [[None, None, None, None, None, None, None, None],
+    board = [
         [None, None, None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None],
-        [None, None, None, None, None, None, None, None]]
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+    ]
 
     column = 0
     row = 0
     for x in fen:
-        if x == 'q':
-            board[row][column] = Queen('b')
+        if x == "q":
+            board[row][column] = Queen("b")
 
-        if x == 'Q':
-            board[row][column] = Queen('w')
+        if x == "Q":
+            board[row][column] = Queen("w")
 
-        if x == 'b':
-            board[row][column] = Bishop('b')
+        if x == "b":
+            board[row][column] = Bishop("b")
 
-        if x == 'B':
-            board[row][column] = Bishop('w')
+        if x == "B":
+            board[row][column] = Bishop("w")
 
-        if x == 'n':
-            board[row][column] = Knight('b')
+        if x == "n":
+            board[row][column] = Knight("b")
 
-        if x == 'N':
-            board[row][column] = Knight('w')
+        if x == "N":
+            board[row][column] = Knight("w")
 
-        if x == 'k':
-            board[row][column] = King('b')
+        if x == "k":
+            board[row][column] = King("b")
 
-        if x == 'K':
-            board[row][column] = King('w')
+        if x == "K":
+            board[row][column] = King("w")
 
-        if x == 'p':
-            board[row][column] = Pawn('b')
+        if x == "p":
+            board[row][column] = Pawn("b")
 
-        if x == 'P':
-            board[row][column] = Pawn('w')
+        if x == "P":
+            board[row][column] = Pawn("w")
 
-        if x == 'r':
-            board[row][column] = Rook('b')
+        if x == "r":
+            board[row][column] = Rook("b")
 
-        if x == 'R':
-            board[row][column] = Rook('w')
+        if x == "R":
+            board[row][column] = Rook("w")
 
         if x.isnumeric():
             column += int(x)
@@ -57,23 +59,25 @@ def loadPos(fen: str):
 
         column += 1
 
-        if x == '/':
+        if x == "/":
             row += 1
             column = 0
 
     return board
 
+
 # Prints chess board to user
 def showBoard(board):
-    board_view = '-------------------------------\n'
+    board_view = "-------------------------------\n"
     for x in board:
         for y in x:
             if y:
-                board_view += ' ' + y.icon + ' '
+                board_view += " " + y.icon + " "
             else:
-                board_view += ' • '
-        board_view += '\n'
+                board_view += " • "
+        board_view += "\n"
     return board_view
-    
-board = loadPos('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
+
+
+board = loadPos("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
 print(showBoard(board))
--- pieces.py
+++ pieces.py
@@ -1,25 +1,29 @@
 from abc import ABC, abstractmethod
 from enum import Enum
 
+
 class Piece(ABC):
     @abstractmethod
     def move(self):
         pass
+
     @abstractmethod
     def genMoves(self):
         pass
+
 
 class Colour(Enum):
     WHITE = 0
     BLACK = 1
 
+
 class Queen(Piece):
     def __init__(self, colour):
         self.colour = colour
-        if self.colour == 'w':
-            self.icon = '♕'
+        if self.colour == "w":
+            self.icon = "♕"
         else:
-            self.icon = '♛'
+            self.icon = "♛"
 
     def move(self):
         pass
@@ -29,73 +33,72 @@ class Queen(Piece):
 
 
 class Rook(Piece):
-
     def __init__(self, colour):
         self.colour = colour
-        if self.colour == 'w':
-            self.icon = '♖'
+        if self.colour == "w":
+            self.icon = "♖"
         else:
-            self.icon = '♜'
+            self.icon = "♜"
 
     def move(self):
         pass
 
     def genMoves(self):
         pass
+
 
 class King(Piece):
-
     def __init__(self, colour):
         self.colour = colour
-        if self.colour == 'w':
-            self.icon = '♔'
+        if self.colour == "w":
+            self.icon = "♔"
         else:
-            self.icon = '♛'
+            self.icon = "♛"
 
     def move(self):
         pass
 
     def genMoves(self):
         pass
+
 
 class Pawn(Piece):
-
     def __init__(self, colour):
         self.colour = colour
-        if self.colour == 'w':
-            self.icon = '♙'
+        if self.colour == "w":
+            self.icon = "♙"
         else:
-            self.icon = '♟︎'
+            self.icon = "♟︎"
 
     def move(self):
         pass
 
     def genMoves(self):
         pass
- 
+
+
 class Knight(Piece):
-
     def __init__(self, colour):
         self.colour = colour
-        if self.colour == 'w':
-            self.icon = '♘'
+        if self.colour == "w":
+            self.icon = "♘"
         else:
-            self.icon = '♞'
+            self.icon = "♞"
 
     def move(self):
         pass
 
     def genMoves(self):
         pass
+
 
 class Bishop(Piece):
-
     def __init__(self, colour):
         self.colour = colour
-        if self.colour == 'w':
-            self.icon = '♗'
+        if self.colour == "w":
+            self.icon = "♗"
         else:
-            self.icon = '♝'
+            self.icon = "♝"
 
     def move(self):
         pass
