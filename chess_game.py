c010437fce71b5df3941a25d82719bb01af5fadb
JackW002 <Ch0nker0fAPassw0rd2169>
2021-11-29 19:50:30 +0000

    add gen_knight method

--- chess_game.py
+++ chess_game.py
@@ -77,17 +77,19 @@ class chessGame:
         moves = 0
         for x in self.board:
             for piece in x:
-                # if type(piece) == King:
-                #     moves = self.gen_king(piece)
-                # if type(piece) == Rook:
-                #     moves = self.gen_rook(piece)
-                # if type(piece) == Bishop:
-                #     moves = self.gen_bishop(piece)
+                if type(piece) == King:
+                    moves = self.gen_king(piece)
+                if type(piece) == Rook:
+                    moves = self.gen_rook(piece)
+                if type(piece) == Bishop:
+                    moves = self.gen_bishop(piece)
                 if type(piece) == Queen:
                     moves = self.gen_queen(piece)
+                if type(piece) == Knight:
+                    move = self.gen_knight(piece)
         return moves
 
-    # Returns possible moves
+    # Generates moves for kings and returns them in list
     def gen_king(self, king: King):
         position = self.piece_pos[king]
 
@@ -118,13 +120,14 @@ class chessGame:
         print(str(king) + str(valid_moves))
         return valid_moves
 
+    # Generates moves for queens and returns them in list
     def gen_queen(self, queen: Queen):
 
         valid_moves = self.gen_rook(queen) + self.gen_bishop(queen)
 
         return valid_moves
 
-
+    # Generates moves for rook and returns them in list
     def gen_rook(self, rook):
 
         pos_x = self.piece_pos[rook][0]
@@ -194,6 +197,7 @@ class chessGame:
 
         return valid_moves
 
+    # Generates moves for bishops and returns them in list
     def gen_bishop(self, bishop):
 
         pos_x = self.piece_pos[bishop][0]
@@ -263,12 +267,70 @@ class chessGame:
 
         return valid_moves
 
+    # Generates moves for knigts and returns them in list
+    def gen_knight(self, knight):
+
+        pos_x = self.piece_pos[knight][0]
+        pos_y = self.piece_pos[knight][1]
+
+        possible_moves = []
+
+        # Check squares above knight
+        if pos_y - 1 >= 0:
+            # Check square to right
+            if pos_x + 2 < 8:
+                possible_moves.append([pos_x+2, pos_y-1])
+            # Check square to left
+            if pos_x - 2 >= 0:
+                possible_moves.append([pos_x-2, pos_y-1])
+
+            if pos_y - 2 >= 0:
+                # Check square to right
+                if pos_x + 1 < 8:
+                    possible_moves.append([pos_x+1, pos_y-2])
+                # Check square to left
+                if pos_x - 1 >= 0:
+                    possible_moves.append([pos_x-1, pos_y-2])
+
+        # Check squares below knight
+        if pos_y + 1 < 8:
+            # Check square to right
+            if pos_x + 2 < 8:
+                possible_moves.append([pos_x+2, pos_y+1])
+            # Check square to left
+            if pos_x - 2 >= 0:
+                possible_moves.append([pos_x-2, pos_y+1])
+            
+            if pos_y + 2 < 8:
+                # Check square to right
+                if pos_x + 1 < 8:
+                    possible_moves.append([pos_x+1, pos_y+2])
+                # Check if square to left
+                if pos_x - 1 >= 0:
+                    possible_moves.append([pos_x-1, pos_y+2])
+
+        valid_moves = []
+
+        for move in possible_moves:
+            if move + [knight.colour] in self.piece_pos.values():
+                continue
+            valid_moves.append(move)
+
+
+        print(str(knight) + str(valid_moves))
+
+
+
+
+
 game = chessGame()
 # game.load_pos("rnbqkbnr/ppp1pppp/8/3p4/7P/7R/PPPPPPP1/RNBQKBN1")
 # game.load_pos("8/8/8/2P1r1P1/8/8/4p3/8")
 # game.load_pos("b2B/8/8/8/8/8/8/8")
 # game.load_pos("8/4P3/5BP1/8/4b2n/2p5/8/8")
-game.load_pos("rn1qkbnr/ppp1pppp/8/3p4/4P1b1/8/PPPP1PPP/RNBQKBNR")
+# game.load_pos("rn1qkbnr/ppp1pppp/8/3p4/4P1b1/8/PPPP1PPP/RNBQKBNR")
+game.load_pos("n6n/8/8/8/8/8/8/n6n")
+# game.load_pos("6n1/8/5Q1p/3P4/8/4N3/6b1/8")
 print(game.show_board())
 game.gen_moves()
 # make dict of all pieces (key) and there corrosponding locations whichh will be updated when piece class moves
